e838f3af8755f5ecee3ab8f098c7500c4fb5183a
Sai Tejas A R <46528244+Saitejas21@users.noreply.github.com>
2021-12-02 19:52:24 +0530

    server deployment

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,21 +1,11 @@
-require("@nomiclabs/hardhat-waffle");
+require('@nomiclabs/hardhat-waffle');
 
-// This is a sample Hardhat task. To learn how to create your own go to
-// https://hardhat.org/guides/create-task.html
-task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
-  const accounts = await hre.ethers.getSigners();
-
-  for (const account of accounts) {
-    console.log(account.address);
-  }
-});
-
-// You need to export an object to set up your config
-// Go to https://hardhat.org/config/ to learn more
-
-/**
- * @type import('hardhat/config').HardhatUserConfig
- */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: '0.8.0',
+  networks: {
+    rinkeby: {
+      url: 'https://eth-rinkeby.alchemyapi.io/v2/oCnETFXQ8M7ZETNzC4Q9cqB6VzVlAhPA',
+      accounts: ['ed48145e91ce388c82adef0f2643dcc63132c766292281a61c49af05fd6d9dc9'],
+    },
+  },
 };
